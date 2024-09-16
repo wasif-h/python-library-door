32263fa381c0c9deff10b547f3c31cd849c5b2c8
Wasif Hossain <wasif.hx@gmail.com>
2024-09-16 19:34:00 +0600

    numpy practice py file add

---
+++ Numpy_python/.ipynb_checkpoints/practicenumpy-checkpoint.py
@@ -0,0 +1,20 @@
+# making the array using loop
+arr = np.zeros([6,5])
+num = 1
+for i in range(6):
+    for j in range(5):
+        arr[i,j] = num
+        num += 1
+arr = arr.astype('int32')
+print("-------- Question Array ----------")
+print(arr,end="\n\n")
+        
+print("-------- Red ----------")
+red_arr = np.array(arr[[0,-2,-1],3:])
+print(red_arr,end="\n\n")
+print("-------- Blue ----------")
+blue_arr = np.array(arr[[2,3],:2])
+print(blue_arr,end="\n\n")
+print("-------- Green ----------")
+green_arr = np.array(arr[[0,1,2,3],[1,2,3,4]])
+print(green_arr,end="\n\n")
